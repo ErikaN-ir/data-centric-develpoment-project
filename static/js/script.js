e418a8019bb900ee6f7f19aa66b724133cd62d31
ErikaN-ir <erika.nethaway@gmail.com>
2020-07-30 21:47:13 +0000

    Added comments to html, css, js files

--- static/js/script.js
+++ static/js/script.js
@@ -1,60 +1,78 @@
+// most of this code from Code Institute tutorial (task manager mini-project)
 $(document).ready(function () {
-    $('.tooltipped').tooltip({delay: 50});
-    $(".button-collapse").sideNav({edge: "left"});
-    $(".collapsible").collapsible();
-    $(".tooltipped").tooltip();
-    $('select').material_select();
+	$('.tooltipped').tooltip({
+		delay: 50
+	});
+	$(".button-collapse").sideNav({
+		edge: "left"
+	});
+	$(".collapsible").collapsible();
+	$(".tooltipped").tooltip();
+	$('select').material_select();
 
-    function set(){
-        let x = 0;
-        sessionStorage.setItem("x", x);
-    }
+	function set() {
+		let x = 0;
+		sessionStorage.setItem("x", x);
+	}
 
-    
-    validateMaterializeSelect();
-    function validateMaterializeSelect() {
-        let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
-        let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
-        if ($("select.validate").prop("required")) {
-            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
-        }
-        $(".select-wrapper input.select-dropdown").on("focusin", function () {
-            $(this).parent(".select-wrapper").on("change", function () {
-                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
-                    $(this).children("input").css(classValid);
-                }
-            });
-        }).on("click", function () {
-            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
-                $(this).parent(".select-wrapper").children("input").css(classValid);
-            } else {
-                $(".select-wrapper input.select-dropdown").on("focusout", function () {
-                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
-                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
-                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
-                        }
-                    }
-                });
-            }
-        });
-    }
-    // https://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea
-    $(document).delegate('#writing', 'keydown', function(e) {
-  var keyCode = e.keyCode || e.which;
 
-  if (keyCode == 9) {
-    e.preventDefault();
-    var start = this.selectionStart;
-    var end = this.selectionEnd;
+	validateMaterializeSelect();
 
-    // set textarea value to: text before caret + tab + text after caret
-    $(this).val($(this).val().substring(0, start)
-                + "\t"
-                + $(this).val().substring(end));
+	function validateMaterializeSelect() {
+		let classValid = {
+			"border-bottom": "1px solid #4caf50",
+			"box-shadow": "0 1px 0 0 #4caf50"
+		};
+		let classInvalid = {
+			"border-bottom": "1px solid #f44336",
+			"box-shadow": "0 1px 0 0 #f44336"
+		};
+		if ($("select.validate").prop("required")) {
+			$("select.validate").css({
+				"display": "block",
+				"height": "0",
+				"padding": "0",
+				"width": "0",
+				"position": "absolute"
+			});
+		}
+		$(".select-wrapper input.select-dropdown").on("focusin", function () {
+			$(this).parent(".select-wrapper").on("change", function () {
+				if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () {})) {
+					$(this).children("input").css(classValid);
+				}
+			});
+		}).on("click", function () {
+			if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+				$(this).parent(".select-wrapper").children("input").css(classValid);
+			} else {
+				$(".select-wrapper input.select-dropdown").on("focusout", function () {
+					if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+						if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+							$(this).parent(".select-wrapper").children("input").css(classInvalid);
+						}
+					}
+				});
+			}
+		});
+	}
+	// https://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea
+	$(document).delegate('#writing', 'keydown', function (e) {
+		var keyCode = e.keyCode || e.which;
 
-    // put caret at right position again
-    this.selectionStart =
-    this.selectionEnd = start + 1;
-  }
-});
+		if (keyCode == 9) {
+			e.preventDefault();
+			var start = this.selectionStart;
+			var end = this.selectionEnd;
+
+			// set textarea value to: text before caret + tab + text after caret
+			$(this).val($(this).val().substring(0, start) +
+				"\t" +
+				$(this).val().substring(end));
+
+			// put caret at right position again
+			this.selectionStart =
+				this.selectionEnd = start + 1;
+		}
+	});
 });
